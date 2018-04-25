a0019b0d1496fbcde588a024144095d85a11c446
Torstein Krause Johansen <torstein@escenic.com>
2018-04-25 13:22:07 +0200

    silly

---
+++ drifting-java/src/main/java/Awesome.java
@@ -0,0 +1,11 @@
+import java.util.List;
+import java.util.Arrays;
+
+
+public class Awesome {
+  public static void main(String[] args) {
+    long systemCurrentTimeMillis = System.currentTimeMillis();
+    List<String> list = Arrays.asList(args);
+    list.clear();
+  }
+}
